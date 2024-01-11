4c6abbdcd0f239559b338fa79d75660494cc43f3
braian <b2414nm@gmail.com>
2024-01-11 15:14:02 -0300

    botones para el carrousel

--- portafolio/js/script.js
+++ portafolio/js/script.js
@@ -1,6 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Función para cambiar el contenido de la sección 1 al hacer clic en un botón
-    document.getElementById('cambiarContenido').addEventListener('click', function () {
-        document.getElementById('contenidoSeccion1').innerHTML = '¡Nuevo contenido de la sección 1!';
-    });
-});
+const slidetrack = document.querySelector(".slide-track")
+const punto = document.querySelectorAll(".punto")
+
+punto.forEach( (cadaPunto, i) =>{
+    punto[i].addEventListener('click',()=>{
+        let posicion = i;
+        let operacion = posicion * -50
+        
+        slidetrack.style.transform = `translateX(${operacion}%)`
+    })
+
+    punto.forEach( (cadaPunto, i) =>{
+        punto[i].classList.remove('activo')
+    })
+    punto[i].classList.add('activo')
+})
